78f3a6d436026558ff6aa4e02b983a66ce924bcc
André Nunes de Lima <andre.nunes@live.com>
2021-03-05 17:47:27 -0300

    Exercício concluído, envolvendo contrutores, palavra this, sobrecarga e encapsulamento

---
+++ ContaBancaria.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace Program120
+{
+    class ContaBancaria
+    {
+        public int NumeroDaConta { get; private set; }
+        public string NomeTitularDaConta { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numeroDaConta, string nomeTitularDaConta)
+        {
+            NumeroDaConta = numeroDaConta;
+            NomeTitularDaConta = nomeTitularDaConta;
+        }
+        public ContaBancaria(int numeroDaConta, string nomeTitularDaConta, double valorDoDeposito) : this(numeroDaConta, nomeTitularDaConta)
+        {
+            Depositar(valorDoDeposito);
+            //Saldo = valorDoDeposito; Esse trecho funciona, mas tira a exclusividade de depositar do metodo Depositar().
+        }
+
+        public void Depositar(double movimentarConta)
+        {
+            Saldo += movimentarConta;
+        }
+        public void Sacar(double movimentarConta)
+        {
+            Saldo -= movimentarConta + 5.00;
+        }
+        public override string ToString()
+        {
+            return "Conta "  
+            + NumeroDaConta
+            + ", Titular: "
+            + NomeTitularDaConta
+            + ", Saldo: $ "
+            + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
